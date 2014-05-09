5afa5e8f07e3548e20d71ba8519595eea8437057
Jan Kotanski <jankotan@gmail.com>
2014-05-09 09:17:55 +0200

    adding end removing files

--- nxselector/State.py
+++ nxselector/State.py
@@ -16,54 +16,48 @@
 #    You should have received a copy of the GNU General Public License
 #    along with nexdatas.  If not, see <http://www.gnu.org/licenses/>.
 ## \package nxselector nexdatas
-## \file Mandatory.py
-# mandatory tab
+## \file State.py
+# automatic tab
 
-""" mandatory tab """
+""" automatic tab """
 
 
 from PyQt4.QtCore import (
     SIGNAL)
 from PyQt4.QtGui import (QHBoxLayout,
-    QGroupBox, QGridLayout,
-    QFrame, QWidgetItem)
+    QGroupBox, QGridLayout, QFrame, QWidgetItem)
 
 from .Element import CPElement
 from .ElementModel import ElementModel
-
 from .Views import CheckerView
 
 import logging
 logger = logging.getLogger(__name__)
 
+
 ## main window class
-class Mandatory(object):
+class State(object):
 
     ## constructor
-    def __init__(self, ui, state = None, userView = CheckerView, 
-                 rowMax=0):
+    # \param settings frame settings
+    def __init__(self, ui, state = None, userView = CheckerView,
+                 rowMax = 0):
         self.ui = ui
         self.state = state
         self.userView = userView
         self.rowMax = rowMax
         self.layout = None
-        
+
+        self.agroup = []
+        self.aview = None
+
         self.mgroup = []
         self.mview = None
-
-    def updateGroups(self):
-        self.mgroup = []
-        mcpgroup = {}
-        for cp in self.state.mcplist:
-            mcpgroup[cp] = True
-        for cp in mcpgroup.keys():
-            self.mgroup.append(
-                CPElement(cp, self.state, group = mcpgroup))
 
 
     def createGUI(self):
 
-        self.ui.mandatory.hide()
+        self.ui.state.hide()
         if self.layout:
             child = self.layout.takeAt(0)
             while child:
@@ -74,16 +68,33 @@ class Mandatory(object):
                     self.layout.removeWidget(child.widget())
                 child = self.layout.takeAt(0)
         else: 
-            self.layout = QHBoxLayout(self.ui.mandatory)
-            
+            self.layout = QHBoxLayout(self.ui.state)
 
-        mframe = QFrame(self.ui.mandatory)
+
+        mframe = QFrame(self.ui.state)
         mframe.setFrameShape(QFrame.StyledPanel)
         mframe.setFrameShadow(QFrame.Raised)
         layout_groups = QHBoxLayout(mframe)
 
         mgroup = QGroupBox(mframe)
-        mgroup.setTitle("")
+        mgroup.setTitle("Automatic")
+        layout_auto = QGridLayout(mgroup)
+        mview = self.userView(mgroup)
+        mview.rowMax = self.rowMax
+
+        layout_auto.addWidget(mview, 0, 0, 1, 1)
+        layout_groups.addWidget(mgroup)
+            
+        self.aview = mview
+        self.layout.addWidget(mframe)
+
+        mframe = QFrame(self.ui.state)
+        mframe.setFrameShape(QFrame.StyledPanel)
+        mframe.setFrameShadow(QFrame.Raised)
+        layout_groups = QHBoxLayout(mframe)
+
+        mgroup = QGroupBox(mframe)
+        mgroup.setTitle("Default")
         layout_auto = QGridLayout(mgroup)
         mview = self.userView(mgroup)
         mview.rowMax = self.rowMax
@@ -93,22 +104,48 @@ class Mandatory(object):
             
         self.mview = mview
         self.layout.addWidget(mframe)
-        self.ui.mandatory.update()
-        if self.ui.tabWidget.currentWidget() == self.ui.mandatory:
-            self.ui.mandatory.show()
+
+        self.ui.state.update()
+        if self.ui.tabWidget.currentWidget() == self.ui.state:
+            self.ui.state.show()
+
+
+    def updateGroups(self):
+        self.agroup = []
+        for cp in self.state.acpgroup.keys():
+            self.agroup.append(
+                CPElement(cp, self.state, 
+                          group = self.state.acpgroup))
+
+        self.mgroup = []
+        mcpgroup = {}
+        for cp in self.state.mcplist:
+            mcpgroup[cp] = True
+        for cp in mcpgroup.keys():
+            self.mgroup.append(
+                CPElement(cp, self.state, group = mcpgroup))
+
 
 
     def setModels(self):
+        md = ElementModel(self.agroup)
+        md.enable = False
+        self.aview.setModel(md)    
+        md.connect(md, SIGNAL("componentChecked"), 
+                   self.__componentChecked)
+
         md = ElementModel(self.mgroup)
         md.enable = False
         self.mview.setModel(md)    
         md.connect(md, SIGNAL("componentChecked"), 
                    self.__componentChecked)
 
+
     def __componentChecked(self):
-        self.ui.mandatory.emit(SIGNAL("componentChecked"))
+        self.ui.state.emit(SIGNAL("componentChecked"))
 
     def updateViews(self):
+        self.aview.reset()
         self.mview.reset()
 
     def reset(self):
