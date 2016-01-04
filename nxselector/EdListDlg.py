20b723bae255092aa3159e53b5f826cbd4993743
Jan Kotanski <jan.kotanski@desy.de>
2016-01-04 12:04:36 +0100

    update copyright

--- nxselector/EdListDlg.py
+++ nxselector/EdListDlg.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #   This file is part of nexdatas - Tango Server for NeXus data writer
 #
-#    Copyright (C) 2014-2015 DESY, Jan Kotanski <jkotan@mail.desy.de>
+#    Copyright (C) 2014-2016 DESY, Jan Kotanski <jkotan@mail.desy.de>
 #
 #    nexdatas is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
