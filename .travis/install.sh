63e24a436a210ce0c8b53329a1b3f4e7d6a5e13e
Jan Kotanski <jankotan@gmail.com>
2019-10-16 15:04:54 +0200

    update sardana version in travis

--- .travis/install.sh
+++ .travis/install.sh
@@ -60,20 +60,20 @@ fi
 if [[ $2 == "2" ]]; then
     echo "install sardana, taurus and nexdatas"
     docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive;  apt-get -qq update; apt-get -qq install -y  nxsconfigserver-db; sleep 10; apt-get -qq install -y python-nxsconfigserver python-nxswriter python-nxstools python-nxsrecselector  python-setuptools'
-    if [[ $1 == "ubuntu18.04" ]]; then
+    if [[ $1 == "debian10" ]]; then
+	docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive;  apt-get -qq update; apt-get -qq install -y python-taurus python-sardana'
+    else
 	docker exec -it --user root ndts /bin/sh -c 'git clone https://github.com/taurus-org/taurus taurus-src; cd taurus-src'
 	docker exec -it --user root ndts /bin/sh -c 'cd taurus-src; python setup.py install'
 	docker exec -it --user root ndts /bin/sh -c 'git clone https://github.com/sardana-org/sardana sardana-src; cd sardana-src'
-	docker exec -it --user root ndts /bin/sh -c 'cd sardana-src; python setup.py install'
-    else
-	docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive;  apt-get -qq update; apt-get -qq install -y python-taurus python-sardana'
+	docker exec -it --user root ndts /bin/sh -c 'cd sardana-src; python setup.py install; git checkout tags/2.8.3 -b b2.8.3'
     fi
 else
     echo "install sardana, taurus and nexdatas"
     docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get install -y  nxsconfigserver-db; sleep 10; apt-get -qq install -y python3-nxsconfigserver python-3nxswriter python3-nxstools python3-nxsrecselector python3-setuptools'
     docker exec -it --user root ndts /bin/sh -c 'git clone https://github.com/taurus-org/taurus taurus-src; cd taurus-src'
     docker exec -it --user root ndts /bin/sh -c 'cd taurus-src; python3 setup.py install'
-    docker exec -it --user root ndts /bin/sh -c 'git clone https://github.com/sardana-org/sardana sardana-src; cd sardana-src; git checkout tags/2.8.3 -b b2.8.3'
+    docker exec -it --user root ndts /bin/sh -c 'git clone https://github.com/sardana-org/sardana sardana-src; cd sardana-src'
     docker exec -it --user root ndts /bin/sh -c 'cd sardana-src; python3 setup.py install'
     
 fi
