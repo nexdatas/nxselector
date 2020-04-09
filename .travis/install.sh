ae535be88a25a1d5567340c4497bbb034a4d2dcb
Jan Kotanski <jankotan@gmail.com>
2020-04-09 09:50:56 +0200

    update tests for focal

--- .travis/install.sh
+++ .travis/install.sh
@@ -83,8 +83,6 @@ if [[ $2 == "2" ]]; then
     docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive;  apt-get -qq update; apt-get -qq install -y  nxsconfigserver-db; sleep 10; apt-get -qq install -y python-nxsconfigserver python-nxswriter python-nxstools python-nxsrecselector  python-setuptools'
     if [[ $1 == "debian10" ]]; then
 	docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive;  apt-get -qq update; apt-get -qq install -y python-taurus python-sardana'
-    elif [[ $1 == "ubuntu20.04" ]]; then
-	docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive;  apt-get -qq update; apt-get -qq install -y python-taurus python-sardana'
     else
 	docker exec -it --user root ndts /bin/sh -c 'git clone https://github.com/taurus-org/taurus taurus-src; cd taurus-src'
 	docker exec -it --user root ndts /bin/sh -c 'cd taurus-src; git checkout tags/4.6.1 -b b4.6.1; python3 setup.py install'
@@ -94,10 +92,14 @@ if [[ $2 == "2" ]]; then
 else
     echo "install sardana, taurus and nexdatas"
     docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get install -y  nxsconfigserver-db; sleep 10; apt-get -qq install -y python3-nxsconfigserver python3-nxswriter python3-nxstools python3-nxsrecselector python3-setuptools'
-    docker exec -it --user root ndts /bin/sh -c 'git clone https://github.com/taurus-org/taurus taurus-src; cd taurus-src'
-    docker exec -it --user root ndts /bin/sh -c 'cd taurus-src; python3 setup.py install'
-    docker exec -it --user root ndts /bin/sh -c 'git clone https://github.com/sardana-org/sardana sardana-src; cd sardana-src'
-    docker exec -it --user root ndts /bin/sh -c 'cd sardana-src; python3 setup.py install'
+    if [[ $1 == "ubuntu20.04" ]]; then
+	docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive;  apt-get -qq update; apt-get -qq install -y python3-taurus python3-sardana'
+    else
+	docker exec -it --user root ndts /bin/sh -c 'git clone https://github.com/taurus-org/taurus taurus-src; cd taurus-src'
+	docker exec -it --user root ndts /bin/sh -c 'cd taurus-src; python3 setup.py install'
+	docker exec -it --user root ndts /bin/sh -c 'git clone https://github.com/sardana-org/sardana sardana-src; cd sardana-src'
+	docker exec -it --user root ndts /bin/sh -c 'cd sardana-src; python3 setup.py install'
+    fi
 fi
 if [ "$?" -ne "0" ]
 then
