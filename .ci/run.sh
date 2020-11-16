dcc215e4dd09e51cf63795a072608d9fea3097ec
Jan Kotanski <jan.kotanski@desy.de>
2020-11-16 11:28:46 +0100

    change .travis to .github/workflows (#32)
    * change .travis to .github/workflows
    
    * fix install.sh script

--- .ci/run.sh
+++ .ci/run.sh
@@ -2,10 +2,10 @@
 
 if [[ $1 == "2" ]]; then
     echo "run python-nxselector"
-    docker exec -it ndts python test
+    docker exec  ndts python test
 else
     echo "run python3-nxselector"
-    docker exec -it ndts python3 test
+    docker exec  ndts python3 test
 fi
 if [ "$?" -ne "0" ]
 then
