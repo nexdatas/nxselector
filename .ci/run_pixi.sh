2b9d1d3aa6432a6b654aa9768eb726580c16f88e
Jan Kotanski <jankotan@gmail.com>
2026-09-10 06:42:37 +0200

    add pixi tests

---
+++ .ci/run_pixi.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "install pixi"
+docker exec  ndts /bin/bash -c 'curl -fsSL https://pixi.sh/install.sh | sh ; export PATH=/var/lib/tango/.pixi/bin:$PATH ; cp .github/workflows/pixi/pixi.toml . ; pixi shell-hook  > .sh.sh ; source .sh.sh ; pixi add   numpy "pytango=10.1.4" setuptools pip wheel argcomplete lxml pytz pyyaml  python-dateutil pninexus fabio h5py matplotlib-base blissdata pytest docutils nxsconfigserver nxswriter pymysql nxstools pymysql sardana nxsrecselector'
+
+echo "run nxselector tests"
+docker exec  ndts /bin/bash -c 'source .sh.sh ;  echo "export MYTANGO_PREFIX=$CONDA_PREFIX/bin" > /home/tango/.env ;   python -m pip install . -vv --no-deps --no-build-isolation; python test'
+
+ERROR=$?
+if [ $ERROR -ne "0" ]
+then
+    echo "ERROR "$ERROR
+    exit 255
+fi
